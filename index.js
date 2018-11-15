3fab8baa1b068a3504dab76091b43f5b55a63ad5
javieramirez90 <javier.ramirez@aiesec.net>
2018-11-15 17:51:20 -0600

    Modal for promo

---
+++ index.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+//listen for a click 
+
+window.addEventListener('mouseover', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', clickOutside);
+//functions
+
+function openModal() {
+  modal.style.display = 'block';
+  removeEvent();
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function clickOutside(e){
+  if(e.target == modal){
+    modal.style.display = 'none'; 
+  }
+}
+function removeEvent() {
+  window.removeEventListener('mouseover', openModal);
+}
